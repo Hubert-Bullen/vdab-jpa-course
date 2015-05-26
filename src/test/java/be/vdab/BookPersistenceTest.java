e0a1e49567de5a6d33426fd141660591402d8a1d
Kevin Van Robbroeck <kevin.vanrobbroeck@gmail.com>
2015-05-26 14:32:19 +0200

    Added unit test setup

---
+++ src/test/java/be/vdab/BookPersistenceTest.java
@@ -0,0 +1,21 @@
+package be.vdab;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class BookPersistenceTest extends AbstractPersistenceTest {
+    @Test
+    public void findAllBooks() throws Exception {
+        List<Book> books = entityManager.createQuery("select b from Book b", Book.class).getResultList();
+        assertEquals("Nineteen Eighty Four", books.get(0).getTitle());
+    }
+
+    @Test
+    public void findAllBooksReturnsExactlyThreeBooks() throws Exception {
+        List<Book> books  = entityManager.createQuery("select b from Book b", Book.class).getResultList();
+        assertEquals(3, books.size());
+    }
+}
