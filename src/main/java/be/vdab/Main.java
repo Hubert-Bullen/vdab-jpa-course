2bf4ed71fc1c73ee30bb382313eebb01c5087e5f
Kevin Van Robbroeck <kevin.vanrobbroeck@gmail.com>
2015-05-27 11:17:46 +0200

    Added some entity manager interactions in the unit tests

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -1,30 +1,41 @@
 package be.vdab;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import java.util.List;
 
 public class Main {
-    private static JpaBookRepository bookRepository;
+    private static EntityManager em;
+
+
+
+
+
+
+
+
+
+
+
+
 
     public static void main(String[] args) {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Books");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
+        Passenger p = em.find(Passenger.class, 1);
+        em.detach(p);
+        someFunkyLogic(p);
 
-        bookRepository = new JpaBookRepository(entityManager);
-        printAllBooks();
+        // p  exists with lastName="Blah"
 
-        entityManager.getTransaction().commit();
-        entityManager.close();
-        entityManagerFactory.close();
+        // db -> id 1 lastname="Holly"
+
+        em.merge(p);
+
+        em.refresh(p);
+
+        em.getTransaction().commit();
+
     }
 
-    private static void printAllBooks() {
-        List<Book> books = bookRepository.findAllBooks();
-        for (Book book : books) {
-            System.out.println(book);
-        }
+    private static void someFunkyLogic(Passenger p) {
+        System.out.println("Funky stuff");
+        p.setLastName("Blah");
     }
 }
--- src/test/java/be/vdab/PassengerPersistenceTest.java
+++ src/test/java/be/vdab/PassengerPersistenceTest.java
@@ -2,23 +2,59 @@ package be.vdab;
 
 import org.junit.Test;
 
+import java.util.Date;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class PassengerPersistenceTest extends AbstractPersistenceTest {
+    /**
+     * TODO: Possible problem: failing test depends on order of other methods (delete).
+     */
     @Test
     public void testFindPassengers() throws Exception {
-        List<Passenger> passengers = entityManager.createQuery("select p from Passenger p", Passenger.class).getResultList();
+        List<Passenger> passengers = entityManager
+                .createQuery("select p from Passenger p", Passenger.class)
+                .getResultList();
+
         assertEquals("Buddy", passengers.get(0).getFirstName());
         assertEquals("Richie", passengers.get(1).getFirstName());
     }
 
     @Test
-    public void testFindPassenger() throws Exception {
-        Passenger buddy = entityManager.createQuery("select p from Passenger p", Passenger.class).getResultList().get(0);
+    public void testFindPassengerById() throws Exception {
+        Passenger buddy = entityManager.find(Passenger.class, 1);
         assertEquals("Holly", buddy.getLastName());
         assertEquals(PassengerType.REGULAR, buddy.getType());
         assertEquals(32, buddy.getAge());
+    }
+
+    @Test
+    public void passengersCanBeCreated() throws Exception {
+        Passenger passenger = new Passenger("Jimi", "Hendrix", PassengerType.OCCASIONAL, new Date(), new byte[]{'d', 'a', 't', 'a'});
+        entityManager.persist(passenger);
+        assertNotNull(passenger.getId());
+    }
+
+    @Test
+    public void passengersCanBeUpdated() throws Exception {
+        Passenger passenger = entityManager.find(Passenger.class, 1);
+        passenger.setFirstName("John");
+    }
+
+    @Test
+    public void passengersAreNotUpdatedWhenDetached() throws Exception {
+        Passenger p = entityManager.find(Passenger.class, 1);
+        assertTrue(entityManager.contains(p));
+        entityManager.detach(p);
+        assertFalse(entityManager.contains(p));
+        p.setFirstName("XXX");
+        entityManager.flush();
+    }
+
+    @Test
+    public void passengerCanBeDeleted() throws Exception {
+        Passenger p = entityManager.getReference(Passenger.class, 2);
+        entityManager.remove(p);
     }
 }
